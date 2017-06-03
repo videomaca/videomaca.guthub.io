5736402ffcc86513fb6dd01c26d46da5310b03ca
videomaca <videomaca@gmail.com>
2017-06-02 21:04:09 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+//ejercicio base 1, taller de intro a la programacino en p5 CADJD puerto varas, junio 2017
+
+//var x = ; //asigna la variable
+
+function setup() {
+createCanvas(800,800);
+background(0,255,0)
+}
+
+function draw() {
+  background(0,255,0)
+  fill(0,0,200);
+  ellipse(mouseX,mouseY,200,200);
+//x = x + 1
+}
